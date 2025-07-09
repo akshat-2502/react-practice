e15f0936649bf14f928f845b8398c57ab1f0b1bc
akshat-2502 <khulbeakshat@gmail.com>
2025-07-10 01:43:58 +0530

    deboincing starts

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ChildComponent = () => {
-  return <div>ChildComponent</div>;
-};
+const ChildComponent = forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      inputRef.current.focus();
+    },
+    enable: () => {
+      inputRef.current.disabled = false;
+    },
+    disable: () => {
+      inputRef.current.disabled = true;
+    },
+    placeholder: () => {
+      inputRef.current.placeholder = "Placeholder Changed";
+    },
+  }));
+
+  return (
+    <div>
+      <input type="text" placeholder="Enter here..." ref={inputRef} />
+    </div>
+  );
+});
 
 export default ChildComponent;
--- src/components/ParentInput.jsx
+++ src/components/ParentInput.jsx
@@ -4,25 +4,31 @@ import ChildComponent from "./ChildComponent";
 const ParentInput = () => {
   const inputRef = useRef();
   return (
-    <div>
+    <div className="mt-10 p-6">
       <ChildComponent ref={inputRef} />
       <button
-        className="px-4 py-2 bg-blue-600 text-white mr-5 rounded-lg"
+        className="px-4 py-2 bg-blue-600 text-white mr-5 rounded-lg cursor-pointer hover:bg-blue-500"
         onClick={() => inputRef.current.focus()}
       >
         Focus
       </button>
       <button
-        className="px-4 py-2 bg-blue-600 text-white mr-5 rounded-lg"
+        className="px-4 py-2 bg-blue-600 text-white mr-5 rounded-lg cursor-pointer hover:bg-blue-500"
         onClick={() => inputRef.current.enable()}
       >
         Enable
       </button>
       <button
-        className="px-4 py-2 bg-blue-600 text-white mr-5 rounded-lg"
+        className="px-4 py-2 bg-blue-600 text-white mr-5 rounded-lg cursor-pointer hover:bg-blue-500"
         onClick={() => inputRef.current.disable()}
       >
         Disable
+      </button>
+      <button
+        className="px-4 py-2 bg-blue-600 text-white mr-5 rounded-lg cursor-pointer hover:bg-blue-500"
+        onClick={() => inputRef.current.placeholder()}
+      >
+        Placeholder
       </button>
     </div>
   );

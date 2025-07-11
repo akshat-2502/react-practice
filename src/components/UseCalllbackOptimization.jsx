546ea919d1381c3d5e753f3625e0a9eb44824bcf
akshat-2502 <khulbeakshat@gmail.com>
2025-07-12 05:15:24 +0530

    usecallback code

--- src/components/UseCalllbackOptimization.jsx
+++ src/components/UseCalllbackOptimization.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+
+const Child = React.memo(({ clickHandle }) => {
+  console.log("Child is rendered");
+  return (
+    <button className="px-5 py-2 bg-blue-500 " onClick={clickHandle}>
+      Click Me
+    </button>
+  );
+});
 
 const UseCalllbackOptimization = () => {
-  return <div>UseCalllbackOptimization</div>;
+  const [count, setCount] = useState(0);
+
+  const handleClick = useCallback(() => {
+    console.log("Button Clicked");
+  }, []); //memoised forever
+
+  return (
+    <div>
+      <input
+        type="number"
+        placeholder="Enter Number"
+        value={count}
+        onChange={(e) => setCount(Number(e.target.value))}
+      />
+      <Child onClick={handleClick} />
+    </div>
+  );
 };
 
 export default UseCalllbackOptimization;

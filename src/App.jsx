1a8c8336098263fdd278b44d5073a1415f7b32c0
akshat-2502 <khulbeakshat@gmail.com>
2025-07-12 13:47:38 +0530

    class based component

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ const App = () => {
         <DisplayUser />
         <UseMemo />
         <UseCalllbackOptimization />
+        <Counter />
       </div>
     </UserProvider>
   );
---
+++ src/components/ClassBasedComponent.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+
+  increment() {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  decrement() {
+    this.setState({ count: this.state.count - 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Class based component</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.increment}>Increase</button>
+        <button onClick={this.decrement}>Decrease</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;

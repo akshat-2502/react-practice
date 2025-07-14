b60c39b980669f9cd5656068b512e4bba1937096
akshat-2502 <khulbeakshat@gmail.com>
2025-07-14 13:59:13 +0530

    pagination in app

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import UseCalllbackOptimization from "./components/UseCalllbackOptimization";
 import ClassBasedCounter from "./components/ClassBasedComponent";
 import HigherOrderComponent from "./components/HigherOrderComponent";
 import HigherOrderComponentLoading from "./components/HigherOrderComponentLoading";
+import Pagination from "./components/Pagination";
 
 const App = () => {
   return (
@@ -35,6 +36,7 @@ const App = () => {
         <ClassBasedCounter />
         <HigherOrderComponent />
         <HigherOrderComponentLoading />
+        <Pagination />
       </div>
     </UserProvider>
   );

45532330eef0a31dc11fbcd1c5cf55533c5b1a64
akshat-2502 <khulbeakshat@gmail.com>
2025-07-15 04:56:49 +0530

    done

--- src/components/DropdownValueInTextbox.jsx
+++ src/components/DropdownValueInTextbox.jsx
@@ -4,15 +4,23 @@ const DropdownValueInTextbox = () => {
   const [dropdownValue, setDropdownValue] = useState("");
 
   return (
-    <select name="Skills">
-      <option value="React">React</option>
-      <option value="Node">Node</option>
-      <option value="Express">Express</option>
-      <option value="MongoDB">MongoDB</option>
-      <option value="Redux">Redux</option>
-      <option value="React-Router">React-Router</option>
-      <option value="JWT">JWT</option>
-    </select>
+    <div className="px-5 py-10">
+      <select
+        className="border p-1 mr-10"
+        onChange={(e) => setDropdownValue(e.target.value)}
+      >
+        <option>Select Skill</option>
+        <option value="React">React</option>
+        <option value="Node">Node</option>
+        <option value="Express">Express</option>
+        <option value="MongoDB">MongoDB</option>
+        <option value="Redux">Redux</option>
+        <option value="React-Router">React-Router</option>
+        <option value="JWT">JWT</option>
+      </select>
+
+      <input type="text" value={dropdownValue} className="border" />
+    </div>
   );
 };
 

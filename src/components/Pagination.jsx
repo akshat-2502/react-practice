34e5207d213a00e0f046e88d0ffe26c7ff0e384c
akshat-2502 <khulbeakshat@gmail.com>
2025-07-14 15:42:52 +0530

    pagination styling

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Pagination = () => {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
     const res = await fetch("https://dummyjson.com/products?limit=100");
@@ -18,13 +19,23 @@ const Pagination = () => {
   return (
     <div>
       {products.length > 0 && (
-        <div>
-          {products.map((item) => (
+        <div className="flex flex-wrap items-center justify-center">
+          {products.slice(page * 10 - 10, page * 10).map((item) => (
             <span>
               <img src={item.thumbnail} alt="" />
               <span>{item.title}</span>
             </span>
           ))}
+        </div>
+      )}
+
+      {products.length > 0 && (
+        <div className="flex justify-center items-center mt-10 pb-10">
+          <span>◀️</span>
+          {[...Array(products.length / 10)].map((_, index) => {
+            return <span key={index}>{index + 1}</span>;
+          })}
+          <span>▶️</span>
         </div>
       )}
     </div>

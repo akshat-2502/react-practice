df4df67b34a2e2dd5b3eaf01d2b087d2a799500a
akshat-2502 <khulbeakshat@gmail.com>
2025-07-14 14:04:50 +0530

    pagination in progress

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Pagination = () => {
+  const [products, setProducts] = useState([]);
+
   const fetchProducts = async () => {
     const res = await fetch("https://dummyjson.com/products?limit=100");
     const data = await res.json();
-    console.log(data);
+    if (data && data.products) {
+      setProducts(data.products);
+    }
   };
 
   useEffect(() => {

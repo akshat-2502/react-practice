aaf1c2112a178d8dd376424932990dfd75dc3e92
akshat-2502 <khulbeakshat@gmail.com>
2025-07-15 16:59:21 +0530

    lazy loaig is inmplemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
-
-import { Route, Router } from "react-router-dom";
 import Home from "./pages/Home";
+
 const About = lazy(() => import("./components/LazyLoading"));
 
 const App = () => {
   return (
-    <Router>
-      <UserProvider>
-        <Suspense fallback={<p>Loading...</p>}>
+    <UserProvider>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<LazyLoading />} />
-        </Suspense>
-      </UserProvider>
-    </Router>
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
+    </UserProvider>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import SearchText from "./components/SearchText";
-import Counter from "./components/Counter";
-import CounterUseReducer from "./components/CounterUseReducer";
-import ParentInput from "./components/ParentInput";
-import DebouncingSearch from "./components/DebouncingSearch";
-import CustomHookParent from "./components/CustomHookParent";
-import ForceUpdate from "./components/ForceUpdate";
-import RemainingText from "./components/RemainingText";
-import Dropdown from "./components/Dropdown";
+import SearchText from "../components/SearchText";
+import Counter from "../components/Counter";
+import CounterUseReducer from "../components/CounterUseReducer";
+import ParentInput from "../components/ParentInput";
+import DebouncingSearch from "../components/DebouncingSearch";
+import CustomHookParent from "../components/CustomHookParent";
+import ForceUpdate from "../components/ForceUpdate";
+import RemainingText from "../components/RemainingText";
+import Dropdown from "../components/Dropdown";
 
-import DisplayUser from "./components/DisplayUser";
-import UseMemo from "./components/UseMemo";
-import UseCalllbackOptimization from "./components/UseCalllbackOptimization";
-import ClassBasedCounter from "./components/ClassBasedComponent";
-import HigherOrderComponent from "./components/HigherOrderComponent";
-import HigherOrderComponentLoading from "./components/HigherOrderComponentLoading";
-import Pagination from "./components/Pagination";
-import DropdownValueInTextbox from "./components/DropdownValueInTextbox";
+import DisplayUser from "../components/DisplayUser";
+import UseMemo from "../components/UseMemo";
+import UseCalllbackOptimization from "../components/UseCalllbackOptimization";
+import ClassBasedCounter from "../components/ClassBasedComponent";
+import HigherOrderComponent from "../components/HigherOrderComponent";
+import HigherOrderComponentLoading from "../components/HigherOrderComponentLoading";
+import Pagination from "../components/Pagination";
+import DropdownValueInTextbox from "../components/DropdownValueInTextbox";
 
 const Home = () => {
   return (
